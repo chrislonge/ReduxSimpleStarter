53d252da1eb6ae593a006aaf11401f1f58c137bd
Chris Longe <attichris@ChrisMacBookPro2017-17.local>
2017-10-20 00:43:37 -0400

    Up to Section 2: Styling with CSS

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = "AIzaSyCNL23nsKQ6Q4RcULZ9nXdi_J_wqVq_324";
 
-// Create a new component. Should produce some HTML.
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null 
+        };
+
+        YTSearch({key: API_KEY, term: 'Messi'}, (videos) => {
+            //ES6 Syntatic Sugar
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0] 
+            }); 
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
